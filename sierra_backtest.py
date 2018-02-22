b009287c31101aa39cfe76020bb89150a32caae8
Rob Thompson <robster970@gmail.com>
2018-02-22 08:58:41 +0000

    Updates to present backtests results in single dataframe.

--- sierra_backtest.py
+++ sierra_backtest.py
@@ -1,4 +1,5 @@
 import logging
+import pandas as pd
 
 
 class Backtest:
@@ -15,26 +16,35 @@ class Backtest:
         # Experiments to sift entire clean dataset for entry and exit criteria to look at backtesting options
         # This involved the creation of a new column time shift percentage change for the following day after a signal
         # This in itself will make the if/else condition in sierra_trade for condition 2 more simple.
-        print("Entry(1) Criteria-------------------------------------------------------------------")
+        # print("Entry(1) Criteria-------------------------------------------------------------------")
         backtest_entry_1 = self.combined[
             (self.combined['VIX_Ndt'] > 0.841) & (self.combined['VIX_Pdf'] > 0) & (self.combined['VIX_Pdf'] < 0.03)]
-        print(backtest_entry_1.iloc[:, [3, 9, 12, 13, 14, 15, 18]].tail(5))
-        print()
+        backtest_entry_1['Action'] = "Entry_1"
+        # print(backtest_entry_1.iloc[:, [3, 9, 12, 13, 14, 15, 18, 19]].tail(5))
+        # print()
 
-        print("Entry(2) Criteria-------------------------------------------------------------------")
+        # print("Entry(2) Criteria-------------------------------------------------------------------")
         backtest_entry_2 = self.combined[
             (self.combined['VIX_NdtY'] > 0.841) & (self.combined['VIX_PdfY'] > -0.03) & (
                         self.combined['VIX_PdfY'] < 0.03) & (
                     self.combined['VIX_Pdf'] < 0)]
-        print(backtest_entry_2.iloc[:, [3, 9, 12, 13, 14, 15, 18]].tail(5))
-        print()
+        backtest_entry_2['Action'] = "Entry_2"
+        # print(backtest_entry_2.iloc[:, [3, 9, 12, 13, 14, 15, 18, 19]].tail(5))
+        # print()
 
-        print("Exit(1) Criteria--------------------------------------------------------------------")
-        backtest_exit = self.combined[
+        # print("Exit(1) Criteria--------------------------------------------------------------------")
+        backtest_exit_1 = self.combined[
             (self.combined['VIX_Ndt'] < 0.159) & (self.combined['VIX_Pdf'] > -0.03) & (self.combined['VIX_Pdf'] < 0.03)]
-        print(backtest_exit.iloc[:, [3, 9, 12, 13, 14, 15, 18]].tail(5))
-        print()
+        backtest_exit_1['Action'] = "Exit_1"
+        # print(backtest_exit_1.iloc[:, [3, 9, 12, 13, 14, 15, 18, 19]].tail(5))
+        # print()
 
-        log_message = "Backtest completed"
+        # Combine the entry and exit decisions and sort by the time-series index
+        backtest_result = pd.concat([backtest_entry_1, backtest_entry_2, backtest_exit_1], axis=0)
+        backtest_result.sort_index(inplace=True)
+
+        # print(backtest_result.iloc[:, [3, 9, 12, 13, 14, 18, 19]].tail(10))
+
+        log_message = "Backtest completed and results returned"
         self.logger.debug(log_message)
-        return
+        return backtest_result
