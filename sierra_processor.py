518738f0953879c7d757d17e72d353d542ddbe73
Rob Thompson <robster970@gmail.com>
2018-02-24 09:58:17 +0000

    Restructured to process valid data sources (Sierra or Quandl) and exception handling for non-valid processor route.

--- sierra_processor.py
+++ sierra_processor.py
@@ -8,7 +8,16 @@ import warnings
 import logging.config
 
 
+# Define class for exception handling used for not having a defined processor
+class MainSierraException(Exception):
+    pass
+
+
+# Initialise variables
 rolling_period = 10
+source = "S"
+es_clean = []
+vix_clean = []
 
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger('sierra_processor')
@@ -18,23 +27,44 @@ logger = logging.getLogger('sierra_processor')
 warnings.filterwarnings("ignore")
 
 # Set up logger
-log_message = "Processing started"
+log_message = "Processing started for: " + source
 logger.info(log_message)
 
-# Import ES data - refactored using Sierra Method call
-es = si.Importer()
-es_clean = es.get_data_sierra("/home/robster970/repo/e-mini/sierrafiles/", "ESH18.dly_BarData.txt", "ES")
-log_message = "ES clean data set ready for use."
-logger.info(log_message)
+if source == "Q":
+    # Making a request to Quandl for ES data
+    log_message = "Getting ES data from Quandl"
+    logger.info(log_message)
+    es = si.Importer()
+    es_clean = es.get_data_quandl("CHRIS/CME_SP1", "ES")
+    log_message = "ES data retrieved from Quandl"
+    logger.info(log_message)
 
-# Import VIX data - refactored using Sierra Method call
-vix = si.Importer()
-vix_clean = vix.get_data_sierra("/home/robster970/repo/e-mini/sierrafiles/", "$VIX.dly_BarData.txt", "VIX")
-log_message = "VIX clean data set ready for use."
-logger.info(log_message)
+    # Making a request to Quandl for VIX data
+    log_message = "Getting VIX data from Quandl"
+    logger.info(log_message)
+    vix = si.Importer()
+    vix_clean = vix.get_data_quandl("CHRIS/CBOE_VX1", "VIX")
+    log_message = "VIX data retrieved from Quandl"
+    logger.info(log_message)
 
-# Tidy up VIX data to remove last two columns for volume and NumberOfTrades
-vix_clean = vix_clean.iloc[:, 0:4]
+elif source == "S":
+    # Import ES data - refactored using Sierra Method call
+    es = si.Importer()
+    es_clean = es.get_data_sierra("/home/robster970/repo/e-mini/sierrafiles/", "ESH18.dly_BarData.txt", "ES")
+    log_message = "ES clean data set ready for use."
+    logger.info(log_message)
+
+    # Import VIX data - refactored using Sierra Method call
+    vix = si.Importer()
+    vix_clean = vix.get_data_sierra("/home/robster970/repo/e-mini/sierrafiles/", "$VIX.dly_BarData.txt", "VIX")
+    log_message = "VIX clean data set ready for use."
+    logger.info(log_message)
+
+    # Tidy up VIX data to remove last two columns for volume and NumberOfTrades
+    vix_clean = vix_clean.iloc[:, 0:4]
+
+else:
+    raise MainSierraException('Invalid processing path: {}'.format(source))
 
 # Create a single combined data frame to work from with NaN rows removed
 combined = pd.concat([es_clean, vix_clean], axis=1)
@@ -84,23 +114,9 @@ es_stop_loss = es_decision.get_stop_loss()
 
 # Run a backtest
 es_backtest_results = sb.Backtest(combined).es_vix_long_test()
+log_message = "Backtest results returned"
+logger.info(log_message)
 print(es_backtest_results.iloc[:, [3, 9, 12, 13, 14, 18, 19]].tail(10))
-
-# Making a request to Quandl for ES data
-log_message = "Getting ES data from Quandl"
-logger.info(log_message)
-es = si.Importer()
-es_clean = es.get_data_quandl("CHRIS/CME_SP1", "ES")
-log_message = "ES data retrieved from Quandl"
-logger.info(log_message)
-
-# Making a request to Quandl for VIX data
-log_message = "Getting VIX data from Quandl"
-logger.info(log_message)
-vix = si.Importer()
-vix_clean = vix.get_data_quandl("CHRIS/CBOE_VX1", "VIX")
-log_message = "VIX data retrieved from Quandl"
-logger.info(log_message)
 
 
 plt.show()
