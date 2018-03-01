e16fdab7432a7fa5aa50fd48a6b94976e12ffa83
Rob Thompson <robster970@gmail.com>
2018-03-01 21:04:20 +0000

    Made changes to add an environment variable SIERRA_ENV to help with build process.

--- sierra_copy.sh
+++ sierra_copy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Update sierra_data in local dev environment
-cd ~/repo/e-mini
-git pull
-cp ~/repo/e-mini/sierrafiles/* ~/PycharmProjects/ES/.sierra_data/
-
-# Update sierra_data in Production environment
-cp /app/e-mini/sierrafiles/* /app/ES/.sierra_data/
+if [ "${SIERRA_ENV}" == "PROD" ]
+then
+   echo "Prod environment variable set"
+   # Update sierra_data in Production environment
+   cp /app/e-mini/sierrafiles/* /app/ES/.sierra_data/
+else
+   echo "Test environment variable set"
+   # Update sierra_data in local dev environment
+   cd ~/repo/e-mini
+   git pull
+   cp ~/repo/e-mini/sierrafiles/* ~/PycharmProjects/ES/.sierra_data/
+fi
