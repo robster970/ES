e7b5885a68f9abc34a78b27c8b7158732c9b1bd3
Rob Thompson <robster970@gmail.com>
2018-03-01 20:28:40 +0000

    Changes to copy files in production environment from e-mini repo

--- sierra_copy.sh
+++ sierra_copy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Update sierra_data in local dev environment
 cd ~/repo/e-mini
 git pull
 cp ~/repo/e-mini/sierrafiles/* ~/PycharmProjects/ES/.sierra_data/
+
+# Update sierra_data in Production environment
+cp /app/e-mini/sierrafiles/* /app/ES/.sierra_data/
