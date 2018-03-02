44a923616cd014345c176a80c978251c749067ec
Rob Thompson <robster970@gmail.com>
2018-03-02 14:18:14 +0000

    Updated sierra_web to pass all returned params needed to render on web page.

--- sierra_web.py
+++ sierra_web.py
@@ -6,7 +6,7 @@ sierra_web = Flask(__name__)
 
 @sierra_web.route("/")
 def main():
-    which = "Q"
+    which = "S"
     response = sierra_processor.main_processor(which)
     run_response = response['RunDate']
     entry_response = response['EntryDecision']
@@ -14,6 +14,8 @@ def main():
     stop_loss_response = response['StopLoss']
     evaluated_data_response = response['EvaluatedData']
     backtest_results_response = response['BacktestResult']
+    evaluated_data_response = evaluated_data_response.to_html(classes='EvaluatedData')
+    backtest_results_response = backtest_results_response.to_html(classes='BacktestResult')
 
     return render_template('index.html', source=which, run=run_response, entry=entry_response, exit=exit_response,
                            stoploss=stop_loss_response, evaluated=evaluated_data_response,
