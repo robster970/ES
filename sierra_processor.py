5fd85b7f2f8565507a718d9a72f4925ec21fb3f1
Rob Thompson <robster970@gmail.com>
2018-03-02 09:29:31 +0000

    Set for Sierra

--- sierra_processor.py
+++ sierra_processor.py
@@ -174,6 +174,6 @@ def main_processor(source):
 
 # Copy most recent sierra files to .sierra_data directory
 os.system('./sierra_copy.sh')
-which = "Q"
+which = "S"
 main_processor(which)
 plt.show(block=False)
