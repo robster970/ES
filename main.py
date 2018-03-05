9b4da8ee9d03d747243e71e731494b3438a21ae1
Rob Thompson <robster970@gmail.com>
2018-03-05 21:49:52 +0000

    Minor mods to travis.yml to comment out snyk, changes to index.html, update of sierra files.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
 import sierra_processor
 
+# Set webapp name to app so it can be used by NGINX docker image
+# Important that filename is set to main.py also
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
+    # Set which data source and run main processor
     which = "S"
     response = sierra_processor.main_processor(which)
+
+    # Handle the data required to render in the index.html template
     run_response = response['RunDate']
     entry_response = response['EntryDecision']
     exit_response = response['ExitDecision']
@@ -17,10 +22,12 @@ def main():
     evaluated_data_response = evaluated_data_response.to_html(classes='EvaluatedData')
     backtest_results_response = backtest_results_response.to_html(classes='BacktestResult')
 
+    # Pass the variables pack into index.html fore rendering
     return render_template('index.html', source=which, run=run_response, entry=entry_response, exit=exit_response,
                            stoploss=stop_loss_response, evaluated=evaluated_data_response,
                            backtest=backtest_results_response)
 
 
+# Start up the webserver
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
