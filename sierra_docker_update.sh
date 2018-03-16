ece77d9c308f9007ea048d827f92fba935234e68
Rob Thompson <robster970@gmail.com>
2018-03-16 18:56:15 +0000

    Shell script to stop and start docker containers.

--- sierra_docker_update.sh
+++ sierra_docker_update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 docker pull robster970/sierra-nginx:latest
-CONTAINER_ID=$(docker ps -a | grep -v Exit | grep 'robster970/sierra-nginx:latest' |awk '{print $1}')
+CONTAINER_ID=$(docker ps -a | grep -v Exit | grep 'sierra-nginx' |awk '{print $1}')
 docker stop $CONTAINER_ID
 docker rm $CONTAINER_ID
 docker run -p 80:5000 -d --restart=always -t robster970/sierra-nginx:latest
+docker ps -a
