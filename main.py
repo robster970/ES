a1ad9bdb4b974c01ee3cb953c6e0cbd4f0239857
Rob Thompson <robster970@gmail.com>
2018-03-17 12:29:56 +0000

    Reformated main.py so that docker image rotation and webhook listener is removed. Also deleted old sierra_copy. Removed sierra_docker_update from this repo too.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
 import sierra_processor
-import json
-import os
 
 
 # Set webapp name to app so it can be used by NGINX docker image
@@ -36,21 +34,6 @@ def create_app():
         return render_template('index.html', source=which, run=run_response, entry=entry_response, exit=exit_response,
                                stoploss=stop_loss_response, evaluated=evaluated_data_response,
                                backtest=backtest_results_response)
-
-    @sierra_app.route('/notifications', methods=['POST'])
-    def notifications():
-        notification_data = json.loads(request.data)
-        print("JSON response from /notification POST: ", notification_data)
-        repo_name = "Brexiteer!"
-        repo_url = "Brexiteer!"
-        # pusher = notification_data['pusher']
-        # repo_url = notification_data['repo_url']
-        # repo_name = notification_data['repo_name']+
-        # print("Validated: "+pusher+", "+repo_url+", "+repo_name)
-        if str(repo_name) == 'robster970/sierra-nginx' and str(
-                repo_url) == 'https://hub.docker.com/r/robster970/sierra-nginx':
-            os.system('./sierra_docker_update.sh')
-        return "OK"
 
     return sierra_app
 
--- sierra_docker_update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo $SIERRA_ENV
-docker pull robster970/sierra-nginx:latest
-CONTAINER_ID=$(docker ps -a | grep -v Exit | grep 'sierra-nginx' |awk '{print $1}')
-docker stop $CONTAINER_ID
-docker rm $CONTAINER_ID
-docker run -p 80:5000 -d --restart=always -t robster970/sierra-nginx:latest
-docker ps -a
