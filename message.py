444259f04fed0a2d0b9fe6422769ddd776f68b33
Rob Thompson <robster970@gmail.com>
2018-03-20 14:49:54 +0000

    Initial messaging script that orchestrates running sierra processor and then passes results to an appropriate method in the messaging class/module.

---
+++ message.py
@@ -0,0 +1,29 @@
+import sierra_processor
+import sierra_messaging as sm
+import logging
+
+
+# Define class for exception handling in main processor
+class MainMessagingException(Exception):
+    pass
+
+
+# Set the data source - this needs to be done in code
+# as there is currently no way of setting this in an accessible way
+which = 'S'
+
+# Run main sierra_processor to get data for message
+response = sierra_processor.main_processor(which)
+
+# Start logger
+logger = logging.getLogger('main_messenger')
+
+# Log for starting main messaging processing
+log_message = "Messaging service started"
+logger.info(log_message)
+
+# Create and send a new message
+new_message = sm.Messaging(response)
+message_response = new_message.ses_aws()
+log_message = "New message created with response: " + message_response
+logger.info(log_message)
