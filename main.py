9b52e780710f4ada76132186617cea44b6629e01
Rob Thompson <robster970@gmail.com>
2018-03-20 14:15:38 +0000

    Added logging to main webserver module.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sierra_processor
+import logging
 
 
 # Set webapp name to app so it can be used by NGINX docker image
@@ -9,6 +10,7 @@ def create_app():
 
     @sierra_app.route("/")
     def main():
+
         which = "S"
         # Set which data source and run main processor
         # Validate whether a URL parameter has been passed in using 'source'
@@ -19,6 +21,12 @@ def create_app():
                 which = "S"
 
         response = sierra_processor.main_processor(which)
+
+        logger = logging.getLogger('main_webserver')
+
+        # Log for start main processing
+        log_message = "Web application server rendering response"
+        logger.info(log_message)
 
         # Handle the data required to render in the index.html template
         run_response = response['RunDate']
