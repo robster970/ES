831efff0f382eb6104efa382975f9ba8d4e64c1d
Rob Thompson <robster970@gmail.com>
2018-03-23 21:01:28 +0000

    Little bit of tidying, additional comments.

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ import logging
 # Set webapp name to app so it can be used by NGINX docker image
 # Important that filename is set to main.py also
 def create_app():
+    # init the object
     sierra_app = Flask(__name__)
 
     # Create a message scheduler to run in the background and start it
     message_scheduler = BackgroundScheduler()
     message_scheduler.start()
 
-    # Create the notification message function
+    # Create the notification function used by the scheduler
     def message_notification():
-        # Set the data source - this needs to be done in code
+        # Set the data source - this needs to be done in code, local scope applies
         # as there is currently no way of setting this in an accessible way
         which = 'S'
 
@@ -31,30 +32,30 @@ def create_app():
         logger.info(log_message)
 
         # Create and send a new message
-        new_message = sm.Messaging(response)
-        message_response = new_message.ses_aws()
+        message_response = sm.Messaging(response).ses_aws()
         log_message = "Message service response: " + message_response
         logger.info(log_message)
 
-    message_scheduler.add_job(message_notification, 'cron', day_of_week='mon-fri', hour=12, minute=40)
+    # Set up the scheduler to use the messaging function using a cron like schedule
+    message_scheduler.add_job(message_notification, 'cron', day_of_week='mon-fri', hour=22, minute=45)
 
+    # Define URLs and associated processing
     @sierra_app.route("/")
     def main():
 
-        which = "S"
         # Set which data source and run main processor
         # Validate whether a URL parameter has been passed in using 'source'
         # Set the default to Sierra if nothing passed or it is invalid
+        which = "S"
         if 'source' in request.args:
             which = request.args['source']
             if which not in ('S', 'Q'):
                 which = "S"
 
+        # Run the processor
         response = sp.main_processor(which)
 
-        # Log for start main processing
-
-        # Handle the data required to render in the index.html template
+        # Handle the data required to render in index.html template
         run_response = response['RunDate']
         last_response = response['LastEvaluated']
         entry_response = response['EntryDecision']
@@ -65,7 +66,7 @@ def create_app():
         evaluated_data_response = evaluated_data_response.to_html(classes='EvaluatedData')
         backtest_results_response = backtest_results_response.to_html(classes='BacktestResult')
 
-        # Pass the variables pack into index.html fore rendering
+        # Pass the variables pack into index.html for rendering
         return render_template('index.html', source=which, run=run_response, last=last_response, entry=entry_response,
                                exit=exit_response, stoploss=stop_loss_response, evaluated=evaluated_data_response,
                                backtest=backtest_results_response)
